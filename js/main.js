24d6f4bfd024407956dbe0629319ea52c7c553c8
wlduddl <71128317+wlduddl@users.noreply.github.com>
2022-11-21 20:14:07 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var swiper = new Swiper('#container', {
+            direction: 'vertical',
+            slidesPerView: 1,
+            mousewheel: true,
+            pagination: {
+                el: '.pager',
+                clickable: true,
+            },
+        });
+
+var swiper = new Swiper('.poList1', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.wpager',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.btnR',
+        prevEl: '.btnL',
+      },
+    });
+
+var typingBool = false; 
+    var typingIdx=0; 
+    var typingTxt = $(".typing-txt").text(); // 타이핑될 텍스트를 가져온다 
+    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+       typingBool=true; 
+       
+       var tyInt = setInterval(typing,100); // 반복동작 
+     } 
+     
+     function typing(){ 
+       if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+         $(".typing").append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+         typingIdx++; 
+       } else{ 
+         clearInterval(tyInt); //끝나면 반복종료 
+       } 
+     }  
+
